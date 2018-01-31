150a01caecf5afe7d78505da622e110f6118552a
Jon Ribeiro <contact@jonathas.com>
2018-01-31 22:25:28 +0100

    Added a flash attribute

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
@@ -15,7 +16,7 @@ import br.com.casadocodigo.loja.models.TipoPreco;
 @Controller
 @RequestMapping("produtos")
 public class ProdutosController {
-	
+
 	@Autowired
 	private ProdutoDAO produtoDao;
 
@@ -23,24 +24,27 @@ public class ProdutosController {
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
-		
+
 		return modelAndView;
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(Produto produto) {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
 		System.out.println(produto);
 		produtoDao.gravar(produto);
-		return "produtos/ok";
+
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+
+		return new ModelAndView("redirect:produtos");
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
 		List<Produto> produtos = produtoDao.listar();
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
 		modelAndView.addObject("produtos", produtos);
-		
+
 		return modelAndView;
 	}
-	
+
 }
