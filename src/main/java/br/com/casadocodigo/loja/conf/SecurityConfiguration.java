f3fbaeec877629e5a123968d0bea2f78dcacca76
Jon Ribeiro <contact@jonathas.com>
2018-02-11 21:27:13 +0100

    Implemented login with User and Roles

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,9 +1,35 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.daos.UsuarioDAO;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UsuarioDAO usuarioDao;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// the order here matters
+		http.authorizeRequests()
+        .antMatchers("/produtos/form").hasRole("ADMIN")
+        .antMatchers("/carrinho").permitAll()
+        .antMatchers("/produtos").hasRole("ADMIN")
+        .antMatchers("/produtos/**").permitAll()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated().and().formLogin();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioDao).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
 }
---
+++ src/main/java/br/com/casadocodigo/loja/models/Usuario.java
@@ -0,0 +1,84 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+	
+	private String password;
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	private List<Role> roles = new ArrayList<>();
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	
+}
