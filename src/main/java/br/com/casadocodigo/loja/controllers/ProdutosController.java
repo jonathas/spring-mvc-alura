c0187400b4ed5027c0d5eda24a724b7244b4491e
Jon Ribeiro <contact@jonathas.com>
2018-02-11 18:53:33 +0100

    Implemented content negotiation

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -80,6 +81,13 @@ public class ProdutosController {
 		modelAndView.addObject("produto", produto);
 		
 		return modelAndView;
+	}
+	
+	// metodo abaixo poderia ser feito se não quisessemos implementar content negotiation
+	@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto detalheJson(@PathVariable("id") Integer id) {
+		return produtoDao.find(id);
 	}
 
 }
