ae3b4fccec5140ce71768f8fc09403eb41a1791c
Jon Ribeiro <jonathas02@gmail.com>
2018-02-14 00:04:56 +0100

    Uncommented the onStartupMethod on ServletSpringMVC configuration

--- src/main/java/br/com/casadocodigo/loja/daos/UsuarioDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/UsuarioDAO.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
